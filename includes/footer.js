9ad3d006ef5944c8e7beef83b9099ea1aa2179ab
KarstenHervoeHansen <97158843+KarstenHervoeHansen@users.noreply.github.com>
2022-01-11 11:18:38 +0100

    Add files via upload

---
+++ includes/footer.js
@@ -0,0 +1,6 @@
+document.write('<footer id="myFooter">')
+document.write('<div class="w3-container w3-theme-l2 w3-padding-32">')
+document.write('<center>DK-Audio powered by www.hansens.dk - DK-3220 Tisvildeleje - Denmark.<BR>Phone:+4531212557<BR>')
+document.write('Please contact Karsten Hansen by mail for questions<BR><A HREF="mailto:kh@hansens.dk">info@hansens.dk</A> - <A HREF="https://www.dk-audio.dk/">www.dk-audio.dk</A><BR></center> ') 	  	
+document.write('</div>')
+document.write('<div class="w3-container w3-theme-l1"><center>Copyright by www.hansens.dk 2018, 2019, 2020<a href="https://www.dk-audio.dk" target="_blank"></a></center></div></footer>')
