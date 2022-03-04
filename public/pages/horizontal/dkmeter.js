e3759870caaa37ba1185efd2e2f399dccee0138f
Karsten Hansen <khh@hansens.dk>
2022-03-04 14:44:53 +0100

    backup

---
+++ public/pages/horizontal/dkmeter.js
@@ -0,0 +1,5 @@
+document.write('<div class="w3-dropdown-hover w3-hide-small">')
+document.write('<button class="w3-large w3-button" title="DK1-DK5">DKMeter <i class="fa fa-caret-down"></i></button>')     
+document.write('<div class="w3-dropdown-content w3-bar-block w3-card-4">')
+document.write('<script src=" dropdowns/list1.js" type="text/javascript"></script>')
+document.write('</div></div>')
